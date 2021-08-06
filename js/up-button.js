9c915593c338c0c40d3d8b2c640b42b58d259083
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-08-06 23:01:02 +0300

    фикс. кнопок

--- js/up-button.js
+++ js/up-button.js
@@ -1,17 +1,20 @@
-(() => {
-    const smoothJumpUp = function() {
-        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
-            window.scrollBy(0,-50);
-            setTimeout(smoothJumpUp, 10);
-        }
+// плавная прокрутка содержимого страницы вверх
+const numLinesScroll = -50;
+const timeOut = 15;
+const linesScrolled = 100;
+
+const smoothJumpUp = function() {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+        window.scrollBy(0,numLinesScroll);
+        setTimeout(smoothJumpUp, timeOut);
     }
-    
-    window.onscroll = function() {
-      const scrolled = window.pageYOffset || document.documentElement.scrollTop;
-      if (scrolled > 100) {
-      	document.getElementById('upbutton').style.display = 'block';
-      } else {
-      	document.getElementById('upbutton').style.display = 'none';
-      }
+}
+
+window.onscroll = function() {
+    const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrolled > linesScrolled) {
+    document.getElementById('linkup').style.display = 'block';
+    } else {
+    document.getElementById('linkup').style.display = 'none';
     }
-})();
+}
