51b2d35e020febc54e731de0be60d9964c8a33ee
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2021-08-06 22:10:41 +0300

    добавил кнопку возврата вверх страницы

---
+++ js/up-button.js
@@ -0,0 +1,17 @@
+(() => {
+    const smoothJumpUp = function() {
+        if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+            window.scrollBy(0,-50);
+            setTimeout(smoothJumpUp, 10);
+        }
+    }
+    
+    window.onscroll = function() {
+      const scrolled = window.pageYOffset || document.documentElement.scrollTop;
+      if (scrolled > 100) {
+      	document.getElementById('upbutton').style.display = 'block';
+      } else {
+      	document.getElementById('upbutton').style.display = 'none';
+      }
+    }
+})();
